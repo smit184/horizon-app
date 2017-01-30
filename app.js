3b0731bac798b336faa0cb20c86b4223981bdb7c
Smit <smitp184@gmail.com>
2017-01-30 14:23:24 -0800

    [added] node commander to app.js

--- app.js
+++ app.js
@@ -1,36 +1,63 @@
+var program = require('commander');
 var args = process.argv;
-var flagsPresent = [];
 
-args.forEach(function(item){
-  if((item === '--sum') || (item === '--product')){
-    flagsPresent.push(item);
-  }
-});
+program
+  .option('-s, --sum', 'sum')
+  .option('-p, --product', 'product')
+  .parse(process.argv);
 
-if(flagsPresent.length !== 1){
-  throw "error: incorrect # of flags present";
+
+
+if(!program.sum && !program.product) {
+	// TO DO: remove after replacing with commander version
+	throw 'error: incorrect number of flags present';
 }
 
-var isNumber = function(x){
-  return !(isNaN(parseFloat(x)));
+var isNumber = function(x) {
+	return !isNaN(parseFloat(x));
 }
 
 var numbers = args.filter(isNumber).map(parseFloat);
 var result;
 
-if(flagsPresent[0]=== '--sum') {
-  result = numbers.reduce(function(a,b){
-    return a + b;
-  });
-}
-if(flagsPresent[0]=== '--product') {
-  result = numbers.reduce(function(a,b){
-    return a * b;
-  });
+if(program.sum) {
+	result = numbers.reduce(function(a,b){ return a + b; });
 }
 
+if(program.product) {
+	result = numbers.reduce(function(a,b){ return a * b; });
+}
 console.log("result is %d", result);
 
+
+// var args = process.argv;
+// var flagsPresent = [];
+//
+// args.forEach(function(item){
+// 	if(item === '--sum' || item === '--product'){
+// 		flagsPresent.push(item);
+// 	}
+// });
+//
+// if(flagsPresent.length !== 1) {
+// 	throw 'error: incorrect number of flags present';
+// }
+//
+// var isNumber = function(x) {
+// 	return !isNaN(parseFloat(x));
+// }
+//
+// var numbers = args.filter(isNumber).map(parseFloat);
+// var result;
+//
+// if(flagsPresent[0] === '--sum') {
+// 	result = numbers.reduce(function(a,b){ return a + b; });
+// }
+//
+// if(flagsPresent[0] === '--product') {
+// 	result = numbers.reduce(function(a,b){ return a * b; });
+// }
+// console.log("result is %d", result);
 
 // if(process.argv.length <= 3){
 // 	throw('error: not enough arguments');
