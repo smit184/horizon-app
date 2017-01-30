52aa1bf0b50e457a7efa0f0674da528eb4ab8144
Smit <smitp184@gmail.com>
2017-01-30 13:49:51 -0800

    [added] changed app.js

---
+++ app.js
@@ -0,0 +1,56 @@
+var args = process.argv;
+var flagsPresent = [];
+
+args.forEach(function(item){
+  if((item === '--sum') || (item === '--product')){
+    flagsPresent.push(item);
+  }
+});
+
+if(flagsPresent.length !== 1){
+  throw "error: incorrect # of flags present";
+}
+
+var isNumber = function(x){
+  return !(isNan(parseFloat(x)));
+}
+
+var numbers = args.filter(isNumber).map(parseFloat);
+var result;
+
+if(flagsPresent[0]=== '--sum') {
+  result = numbers.reduce(function(a,b){
+    return a + b;
+  });
+}
+if(flagsPresent[0]=== '--product') {
+  result = numbers.reduce(function(a,b){
+    return a * b;
+  });
+}
+
+console.log("result is %d", result);
+
+
+// if(process.argv.length <= 3){
+// 	throw('error: not enough arguments');
+// }
+//
+// if(process.argv[2] === '--sum' || process.argv[2] === '--product'){
+// 	if(process.argv[2] === '--sum'){
+// 		var sum=0;
+// 		for(var i=3; i<process.argv.length; i++){
+// 			sum += parseFloat(process.argv[i]);
+// 		}
+// 		console.log(sum);
+//
+// 	}else{
+// 		var prod = 1;
+// 		for(var i=3; i<process.argv.length; i++){
+// 			prod *= parseFloat(process.argv[i]);
+// 		}
+// 		console.log(prod);
+// 	}
+// } else{
+//   throw ('error: no flag was given');
+// }
